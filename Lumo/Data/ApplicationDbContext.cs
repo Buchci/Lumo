e164251894730d55340a53aef22c06be2698be59
Buchi <dawid.buchci@gmail.com>
2025-09-16 00:49:26 +0200

    Added Tags

--- Lumo/Data/ApplicationDbContext.cs
+++ Lumo/Data/ApplicationDbContext.cs
@@ -10,5 +10,30 @@ namespace Lumo.Data
             : base(options)
         {
         }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Unikalność nazwy w obrębie użytkownika
+            builder.Entity<Tag>()
+                .HasIndex(t => new { t.ResourceKey, t.UserId, t.CustomName})
+                .IsUnique();
+
+            // Relacja: User → Tag
+            builder.Entity<Tag>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Dane początkowe (systemowe tagi)
+            builder.Entity<Tag>().HasData(
+                new Tag { Id = 1, ResourceKey = "Tag.Work", IsGlobal = true },
+                new Tag { Id = 2, ResourceKey = "Tag.Family", IsGlobal = true },
+                new Tag { Id = 3, ResourceKey = "Tag.Health", IsGlobal = true }
+            );
+        }
     }
 }
---
+++ Lumo/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿namespace Lumo.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public string? ResourceKey { get; set; }
+
+        public string? CustomName { get; set; }
+
+        public bool IsGlobal { get; set; }
+
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+    }
+
+}
